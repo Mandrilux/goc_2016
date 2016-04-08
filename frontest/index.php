a39ec983bc63a503762706ce5f7ea9eb7c12cdd3
Corentin Grandmaire <corentin.grndmaire@epitech.eu>
2016-04-09 01:01:12 +0200

    lag

--- frontest/index.php
+++ frontest/index.php
@@ -56,9 +56,11 @@ include_once('../modele/connexion_sql.php'); ?>
       <div class="row">
         <div class=".col-xs-4 .col-sm-8 .col-md-6 .col-md-offset-3">
           <h3 class="section-title multiple-title">What is Our Goal?</h3>
-          <p class="text-center">Our goal is to make your everyday life easier</p>
+          <p class="text-center">Make your everyday life easier</p>
           <p class="text-center">
-            <b>Something</b>
+          <b>Find the closest and fastest way to travel.</b><br>
+          <b>Use your location to provide a plan to your destination.</b><br>
+          <b></b><br>
           </p>
         </div>
         <!-- /.col-sm-6 -->
@@ -86,150 +88,6 @@ include_once('../modele/connexion_sql.php'); ?>
       <!-- row -->
     </div>
     <!-- container -->
-  </section>
-
-  <section id="speakers" class="section speakers">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-
-          <h3 class="section-title">Speakers</h3>
-
-        </div>
-      </div>
-
-      <div class="row">
-        <div class="col-md-4">
-          <div class="speaker">
-
-            <figure>
-              <img alt="" class="img-responsive center-block" src="assets/images/speakers/speaker-1.jpg">
-            </figure>
-
-            <h4>Jhon Smith</h4>
-
-            <p>CEO of Peren</p>
-
-            <ul class="social-block">
-              <li><a href=""><i class="ion-social-twitter"></i></a></li>
-              <li><a href=""><i class="ion-social-facebook"></i></a></li>
-              <li><a href=""><i class="ion-social-linkedin-outline"></i></a></li>
-              <li><a href=""><i class="ion-social-googleplus"></i></a></li>
-            </ul>
-
-          </div>
-          <!-- /.speaker -->
-        </div>
-        <!-- /.col-md-4 -->
-
-        <div class="col-md-4">
-          <div class="speaker">
-
-            <figure>
-              <img alt="" class="img-responsive center-block" src="assets/images/speakers/speaker-2.jpg">
-            </figure>
-
-            <h4>Jhon Smith</h4>
-
-            <p>CEO of Peren</p>
-
-            <ul class="social-block">
-              <li><a href=""><i class="ion-social-twitter"></i></a></li>
-              <li><a href=""><i class="ion-social-facebook"></i></a></li>
-              <li><a href=""><i class="ion-social-linkedin-outline"></i></a></li>
-              <li><a href=""><i class="ion-social-googleplus"></i></a></li>
-            </ul>
-
-          </div>
-          <!-- /.speaker -->
-        </div>
-        <!-- /.col-md-4 -->
-
-        <div class="col-md-4">
-          <div class="speaker">
-
-            <figure>
-              <img alt="" class="img-responsive center-block" src="assets/images/speakers/speaker-3.jpg">
-            </figure>
-
-            <h4>Jhon Smith</h4>
-
-            <p>CEO of Peren</p>
-
-            <ul class="social-block">
-              <li><a href=""><i class="ion-social-twitter"></i></a></li>
-              <li><a href=""><i class="ion-social-facebook"></i></a></li>
-              <li><a href=""><i class="ion-social-linkedin-outline"></i></a></li>
-              <li><a href=""><i class="ion-social-googleplus"></i></a></li>
-            </ul>
-
-          </div>
-          <!-- /.speaker -->
-        </div>
-        <!-- /.col-md-4 -->
-      </div>
-      <!-- /.row -->
-
-      <div class="row">
-        <div class="col-md-4">
-
-          <div class="speaker">
-
-            <figure>
-              <img alt="" class="img-responsive center-block" src="assets/images/speakers/speaker-4.jpg">
-            </figure>
-
-            <h4>Jhon Smith</h4>
-
-            <p>CEO of Peren</p>
-
-            <ul class="social-block">
-              <li><a href=""><i class="ion-social-twitter"></i></a></li>
-              <li><a href=""><i class="ion-social-facebook"></i></a></li>
-              <li><a href=""><i class="ion-social-linkedin-outline"></i></a></li>
-              <li><a href=""><i class="ion-social-googleplus"></i></a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-md-4">
-
-          <div class="speaker">
-
-            <figure>
-              <img alt="" class="img-responsive center-block" src="assets/images/speakers/speaker-5.jpg">
-            </figure>
-
-            <h4>Jhon Smith</h4>
-
-            <p>CEO of Peren</p>
-
-            <ul class="social-block">
-              <li><a href=""><i class="ion-social-twitter"></i></a></li>
-              <li><a href=""><i class="ion-social-facebook"></i></a></li>
-              <li><a href=""><i class="ion-social-linkedin-outline"></i></a></li>
-              <li><a href=""><i class="ion-social-googleplus"></i></a></li>
-            </ul>
-
-          </div>
-        </div>
-
-        <div class="col-md-4">
-          <div class="speaker">
-            <figure>
-              <img alt="" class="img-responsive center-block" src="assets/images/speakers/speaker-6.jpg">
-            </figure>
-            <h4>Jhon Smith</h4>
-            <p>CEO of Peren</p>
-            <ul class="social-block">
-              <li><a href=""><i class="ion-social-twitter"></i></a></li>
-              <li><a href=""><i class="ion-social-facebook"></i></a></li>
-              <li><a href=""><i class="ion-social-linkedin-outline"></i></a></li>
-              <li><a href=""><i class="ion-social-googleplus"></i></a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
   </section>
 
   <section id="registration" class="section registration">
