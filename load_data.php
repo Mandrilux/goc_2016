1771bb888461f28d1ef0feba75b352818ad7d5e7
picot_a <vincent1.picot@epitech.eu>
2016-04-08 21:16:45 +0200

    maj

--- load_data.php
+++ load_data.php
@@ -3,7 +3,7 @@
 include('modele/connexion_sql.php');
 
 $list_station = file_get_contents('https://developer.jcdecaux.com/rest/vls/stations/Luxembourg.json');
-vardump($list_station);
-$rep = $bdd->prepare()
+var_dump($list_station);
+//$rep = $bdd->prepare()
 
 ?>
