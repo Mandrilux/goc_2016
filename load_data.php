9e973cbd570f8913495f9cacae04822612cb4542
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-08 22:16:15 +0200

    init

--- load_data.php
+++ load_data.php
@@ -3,7 +3,6 @@
 include('modele/connexion_sql.php');
 
 $list_station = file_get_contents('https://developer.jcdecaux.com/rest/vls/stations/Luxembourg.json');
-//var_dump($list_station);
+var_dump($list_station);
 //$rep = $bdd->prepare()
-echo $list_station[0];
 ?>
