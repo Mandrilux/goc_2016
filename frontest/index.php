fab7241068456dc79e21c610975ad907e3f1588f
Corentin Grandmaire <corentin.grndmaire@epitech.eu>
2016-04-09 01:58:24 +0200

    lag

--- frontest/index.php
+++ frontest/index.php
@@ -121,8 +121,10 @@ include_once('../modele/connexion_sql.php'); ?>
           </div>
       </form>
       </div>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96706.50013548559!2d-78.9870674333782!3d40.76030630398601!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited+States!5e0!3m2!1sen!2sbd!4v1436299406518"
+      <div class="">
+      <iframe src="https://www.google.com/maps/embed/v1/view?zoom=12&center=49.6116%2C6.1319&key=AIzaSyDyASVb6Re14qqRDWDxs7PJ3mmouNCxIfs" 
         width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
   </section>
 
   <section id="contribution" class="section bg-image-2 contribution">
