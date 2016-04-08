589955fdf6e3a872b98a44d0bcb37d2c8885bf08
Corentin Grandmaire <corentin.grndmaire@epitech.eu>
2016-04-09 01:06:18 +0200

    lag

--- frontest/index.php
+++ frontest/index.php
@@ -105,50 +105,12 @@ include_once('../modele/connexion_sql.php'); ?>
           </div>
           <div class="col-sm-6">
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="First Name" id="fname" name="fname" required>
+              <input type="text" class="form-control" placeholder="Longitude" id="fname" name="fname" required>
             </div>
 
             <div class="form-group">
-              <input type="text" class="form-control" placeholder="Last Name" id="lname" name="lname" required>
+              <input type="text" class="form-control" placeholder="Latitude" id="lname" name="lname" required>
             </div>
-
-            <div class="form-group">
-              <input type="email" class="form-control" placeholder="Email" id="email" name="email" required>
-            </div>
-
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Phone" id="cell" name="cell" required>
-            </div>
-          </div>
-
-          <div class="col-sm-6">
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Address" id="address" name="address" required>
-            </div>
-
-            <div class="form-group">
-              <input type="text" class="form-control" placeholder="Zip Code" id="zip" name="zip" required>
-            </div>
-
-            <div class="form-group">
-              <select class="form-control" name="city" id="city" required>
-                <option readonly>City</option>
-                <option>City Name 1</option>
-                <option>City Name 2</option>
-                <option>City Name 3</option>
-                <option>City Name 4</option>
-              </select>
-            </div>
-
-            <div class="form-group">
-              <select class="form-control" name="program" id="program" required>
-                <option readonly>Select Your Program</option>
-                <option>Program Name 1</option>
-                <option>Program Name 2</option>
-                <option>Program Name 3</option>
-              </select>
-            </div>
-          </div>
         </div>
         <div class="text-center mt20">
           <button type="submit" class="btn btn-black" id="registration-submit-btn">Submit</button>
