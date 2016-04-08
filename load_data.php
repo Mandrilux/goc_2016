a9e050d8c11d80ac2cbf492100e7512ed5928a65
picot_a <vincent1.picot@epitech.eu>
2016-04-09 00:42:56 +0200

    maj

--- load_data.php
+++ load_data.php
@@ -11,13 +11,25 @@ $list_station_dec = json_decode($list_station);
 //print_r($list_station_dec);
 foreach($list_station_dec as $data)
 {
-//  print($data->{'number'});
- $rep = $bdd->prepare('INSERT INTO `stations_velo`(`id`, `nom`, `adresse`, `latitude`, `longitude`) VALUES (:id,:nom,:adresse,:latitude,:longitude)');
-$rep->execute(array('id'=>$data->{'number'},
-		    'nom'=>$data->{'name'},
-		    'adresse'=>$data->{'address'},
-		    'latitude'=>$data->{'latitude'},
-		    'longitude'=>$data->{'longitude'}));
-
+  //  print($data->{'number'});
+  $id = $bdd->prepare('SELECT count(id) AS nb FROM `stations_velo` WHERE `id` = :id');
+  $is_id_exist = $id->fetch();
+  if ($is_id_exist['nb'] > 0)
+  {
+    $rep = $bdd->prepare('UPDATE `stations_velo` SET `nom` = :nom, `adresse` = :adresse, `latitude` = :latitude, `longitude` = :longitude');
+    $rep->execute(array('nom'=>$data->{'name'},
+			'adresse'=>$data->{'address'},
+			'latitude'=>$data->{'latitude'},
+			'longitude'=>$data->{'longitude'}));    
+  }
+  else
+  {
+    $rep = $bdd->prepare('INSERT INTO `stations_velo`(`id`, `nom`, `adresse`, `latitude`, `longitude`) VALUES (:id,:nom,:adresse,:latitude,:longitude)');
+    $rep->execute(array('id'=>$data->{'number'},
+			'nom'=>$data->{'name'},
+			'adresse'=>$data->{'address'},
+			'latitude'=>$data->{'latitude'},
+			'longitude'=>$data->{'longitude'}));
+  }
 }
 ?>
