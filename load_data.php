a9f8de0a404c45f5c011a3d3bce4b14111696a33
picot_a <vincent1.picot@epitech.eu>
2016-04-08 23:15:37 +0200

    maj

--- load_data.php
+++ load_data.php
@@ -5,14 +5,19 @@ include('modele/connexion_sql.php');
 $list_station = file_get_contents('https://developer.jcdecaux.com/rest/vls/stations/Luxembourg.json');
 $list_station_dec = json_decode($list_station);
 
-print($list_station_dec[0]->{'number'});
-//print_r($list_station_dec);
 
-//$rep = $bdd->prepare('INSERT INTO `stations_velo`(`id`, `nom`, `adresse`, `latitude`, `longitude`) VALUES (:id,:nom,:adresse,:latitude,:longitude)');
+
+
+//print_r($list_station_dec);
+foreach($list_station_dev as $data)
+{
+  print($data->{'number'});
+  //$rep = $bdd->prepare('INSERT INTO `stations_velo`(`id`, `nom`, `adresse`, `latitude`, `longitude`) VALUES (:id,:nom,:adresse,:latitude,:longitude)');
 /*$rep->execute(array('id'=>1,
 		    'nom'=>"lol",
 		    'adresse'=>"mdr",
 		    'latitude'=>1.5,
 		    'longitude'=>5.1));
-*/
+ */
+}
 ?>
