4e7cb111eb1c4cc055f241315a52dce6a3f006ef
picot_a <vincent1.picot@epitech.eu>
2016-04-08 23:17:17 +0200

    maj

--- load_data.php
+++ load_data.php
@@ -9,7 +9,7 @@ $list_station_dec = json_decode($list_station);
 
 
 //print_r($list_station_dec);
-foreach($list_station_dev as $data)
+foreach($list_station_dec as $data)
 {
   print($data->{'number'});
   //$rep = $bdd->prepare('INSERT INTO `stations_velo`(`id`, `nom`, `adresse`, `latitude`, `longitude`) VALUES (:id,:nom,:adresse,:latitude,:longitude)');
