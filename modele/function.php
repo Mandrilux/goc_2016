a851ff288200dab3e688c4dbfc7a43927293c7df
lucas duval <lucas.duval@epitech.eu>
2016-04-09 00:58:56 +0200

    update

--- modele/function.php
+++ modele/function.php
@@ -3,9 +3,22 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+function curl_get_file_contents($URL)
+{
+  $c = curl_init();
+  curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($c, CURLOPT_URL, $URL);
+  $contents = curl_exec($c);
+  curl_close($c);
+
+  if ($contents) return $contents;
+  else return FALSE;
+}
+}
+
 function get_info_station($id)
 {
-  $dataJ = file_get_contents('https://api.jcdecaux.com/vls/v1/stations/'.$id.'?contract=Luxembourg&apiKey=58a7596376f3ae8c4af270a5abc6b7c04ecff44c');
+  $dataJ = curl_get_file_contents('https://api.jcdecaux.com/vls/v1/stations/'.$id.'?contract=Luxembourg&apiKey=58a7596376f3ae8c4af270a5abc6b7c04ecff44c');
   $data = json_decode($dataJ);
   $new_data = array('banking' => $data->{'banking'},
 		    'status' => $data->{'status'},
