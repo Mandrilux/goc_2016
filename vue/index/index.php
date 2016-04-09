332e73f96c31406a223a0fc129c412d324b4e9ba
Corentin Grandmaire <corentin.grndmaire@epitech.eu>
2016-04-09 05:50:47 +0200

    finsite

--- vue/index/index.php
+++ vue/index/index.php
@@ -89,24 +89,24 @@
           <h3><div id="bus"></div><br>Bus Stop</h3>
         </div>
         <script>
-        var easingFn = function (t, b, c, d) {
-          var ts = (t /= d) * t;
-          var tc = ts * t;
-          return b + c * (tc * ts + -5 * ts * ts + 10 * tc + -10 * ts + 5 * t);
-        }
-        var options = {
-          useEasing : true,
-          easingFn: easingFn,
-          useGrouping : true,
-          separator : ',',
-          decimal : '.'
-        }
-        var velo = new CountUp("velo", 0, <?php echo get_all_velo(); ?>, 0, 2, options);
-        var station = new CountUp("station", 0, <?php echo get_total_station(); ?>, 0, 2, options);
-        var bus = new CountUp("bus", 0, 800, 0, 2, options);
-        velo.start();
-        station.start();
-        bus.start();
+          var easingFn = function(t, b, c, d) {
+            var ts = (t /= d) * t;
+            var tc = ts * t;
+            return b + c * (tc * ts + -5 * ts * ts + 10 * tc + -10 * ts + 5 * t);
+          }
+          var options = {  
+            useEasing: true,
+              easingFn: easingFn,
+              useGrouping: true,
+              separator: ',',
+              decimal: '.'
+          }
+          var velo = new CountUp("velo", 0, <?php echo get_all_velo(); ?>, 0, 2, options);
+          var station = new CountUp("station", 0, <?php echo get_total_station(); ?>, 0, 2, options);
+          var bus = new CountUp("bus", 0, 800, 0, 2, options);
+          velo.start();
+          station.start();
+          bus.start();
         </script>
       </div>
       <!-- row -->
@@ -121,7 +121,7 @@
           <h3 class="section-title">Find the closest station</h3>
         </div>
       </div>
-       <form action="index.php" method="post" id="registration-form">
+      <form action="index.php" method="post" id="registration-form">
         <div class="row">
           <div class="col-md-12" id="registration-msg" style="display:none;">
             <div class="alert"></div>
@@ -144,31 +144,30 @@
       <div class="container col-md-12">
         <br>
         <script>
-        var myCenter=new google.maps.LatLng(51.508742,-0.120842);
-        var station=new google.maps.LatLng(51.50870,-0.120852);
+          var myCenter = new google.maps.LatLng(51.508742, -0.120842);
+          var station = new google.maps.LatLng(51.50870, -0.120852);
 
-        function initialize()
-        {
-        var mapProp = {
-          center:myCenter,
-          zoom:12,
-          mapTypeId:google.maps.MapTypeId.ROADMAP
-          };
+          function initialize() {
+            var mapProp = {
+              center: myCenter,
+              zoom: 12,
+              mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
 
-        var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+            var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-        var marker=new google.maps.Marker({
-          position:myCenter,
-          });
-        var marker2=new google.maps.Marker({
-          position:station,
-          });
+            var marker = new google.maps.Marker({
+              position: myCenter,
+            });
+            var marker2 = new google.maps.Marker({
+              position: station,
+            });
 
-          marker.setMap(map);
-          marker2.setMap(map);
-        }
+            marker.setMap(map);
+            marker2.setMap(map);
+          }
 
-        google.maps.event.addDomListener(window, 'load', initialize);
+          google.maps.event.addDomListener(window, 'load', initialize);
         </script>
         <div id="googleMap" style="width:100%;height:450px;"></div>
       </div>
@@ -182,7 +181,6 @@
           <p>For 1 miles using a car: 0.099 kg/km CO2 rejected, using a bus: 0.014 kg/km CO2 rejected</p>
           <p>Using VeloH you loose approximately 40 calories per miles</p>
 
-          <a class="btn btn-white" href="#">Submit</a>
         </div>
       </div>
     </div>
