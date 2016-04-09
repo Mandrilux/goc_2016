0ad8c0d245f17c723089e0aab16cc563fca36c07
lucas duval <lucas.duval@epitech.eu>
2016-04-09 06:04:19 +0200

    key

--- modele/function.php
+++ modele/function.php
@@ -66,7 +66,7 @@ function get_closest_station($longitude, $latitude)
     {
       $dist = my_get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='.$latitude.','.$longitude.'&destinations='.$data2["latitude"].','.$data2["longitude"].'&key=AIzaSyB0-r4_Rs05QuKWSMQml-Z41wplA-YRkIo');
       $dist = json_decode($dist);
-      //var_dump($dist);
+      var_dump($dist);
       $cur_dist = $dist->{'rows'}[0]->{'elements'}[0]->{'distance'}->{'value'};
       if (isset($min_dist) == false)
       {
