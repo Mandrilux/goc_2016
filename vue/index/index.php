ef776ad830483b659a60b6c332ceff6938c7e15d
Corentin Grandmaire <corentin.grndmaire@epitech.eu>
2016-04-09 12:51:33 +0200

    info2

--- vue/index/index.php
+++ vue/index/index.php
@@ -123,7 +123,7 @@ if (navigator.geolocation)
     <div class="container">
       <div class="row">
         <div class="col-md-12">
-          <h3 class="section-title multiple-title">Find the closest station</h3>
+          <center><h3 class="section-title">Find the closest station</h3></center>
         </div>
         <br>
       </div>
