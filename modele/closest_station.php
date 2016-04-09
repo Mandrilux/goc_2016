83064d77cf8cd4da461cbf05a9936e42e0d3a48e
picot_a <vincent1.picot@epitech.eu>
2016-04-09 03:01:17 +0200

    maj

--- modele/closest_station.php
+++ modele/closest_station.php
@@ -13,7 +13,7 @@ function get_close($longitude, $latitude)
   while ($data2 = $data->fetch())
   {*/
   $data2 = $data->fetch();
-  $dist = my_get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='.$latitude.','.$longitude.'&destinations='.$data2["latitude"].','.$data2["longitude"].'&key=AIzaSyB6jHXQvTl5A3MGRyzgrAywkPvGKqzkobc');
+  $dist = my_get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='.$latitude.','.$longitude.'&destinations='.$data2["latitude"].','.$data2["longitude"].'&key=AIzaSyA16k_WmmO9gJG02IukDjHCQEeTYQgGbyw');
   echo $dist;
   //}
   return $dist;
