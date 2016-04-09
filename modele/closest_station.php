984c6266cc8f42e01b97a25eee8428ab58461bfe
picot_a <vincent1.picot@epitech.eu>
2016-04-09 05:06:53 +0200

    maj

--- modele/closest_station.php
+++ modele/closest_station.php
@@ -12,34 +12,40 @@ function get_closest_station($longitude, $latitude)
   $tab_data = array();
   while ($data2 = $data->fetch())
     array_push($tab_data, $data2);
-  foreach($tab_data as $data2)
+  $stop = 1;
+  while ($stop == 1)
   {
-    echo "lol";
-    $dist = my_get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='.$latitude.','.$longitude.'&destinations='.$data2["latitude"].','.$data2["longitude"].'&key=AIzaSyC5WbmOFch6mj7T1L6CXjRMJ0sjdJuFlpc');
-    $dist = json_decode($dist);
-    $cur_dist = $dist->{'rows'}[0]->{'elements'}[0]->{'distance'}->{'value'};
-    if (isset($min_dist) == false)
+    $stop = 0;
+    foreach($tab_data as $data2)
     {
-      $id_min_dist = $data2['id'];
-      $min_dist = $cur_dist;
+      //echo "lol";
+      $dist = my_get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='.$latitude.','.$longitude.'&destinations='.$data2["latitude"].','.$data2["longitude"].'&key=AIzaSyC5WbmOFch6mj7T1L6CXjRMJ0sjdJuFlpc');
+      $dist = json_decode($dist);
+      $cur_dist = $dist->{'rows'}[0]->{'elements'}[0]->{'distance'}->{'value'};
+      if (isset($min_dist) == false)
+      {
+	$id_min_dist = $data2['id'];
+	$min_dist = $cur_dist;
+      }
+      elseif ($min_dist > $cur_dist) 
+      {
+	$id_min_dist = $data2['id'];
+	$min_dist = $cur_dist;
+      }
     }
-    elseif ($min_dist > $cur_dist) 
+    //echo $min_dist;
+    //echo $id_min_dist;
+    $info = get_info_station($id_min_dist);
+    if ($info['dispo_velo'] == 0)
     {
-      $id_min_dist = $data2['id'];
-      $min_dist = $cur_dist;
-    }
-  }
-  echo $min_dist;
-  echo $id_min_dist;
-  $info = get_info_station($id_min_dist);
-  if ($info['dispo_velo'] == 0)
-  {
-    $i = 0;
-    while($tab_data[$i])
-    {
-      if ($tab_data[$i]['id'] == $id_min_dist)
-	unset($tab_data[$i]);
-      $i++;
+      $i = 0;
+      $stop = 1;
+      while($tab_data[$i])
+      {
+	if ($tab_data[$i]['id'] == $id_min_dist)
+     	  unset($tab_data[$i]);
+	$i++;
+      }
     }
   }
 }
