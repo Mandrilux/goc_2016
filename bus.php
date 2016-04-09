77698175786d43ee6c4aba47c5db0c1404e3425e
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 12:52:15 +0200

    fix

--- bus.php
+++ bus.php
@@ -1,5 +1,5 @@
 <?php
-include_once("modele/function.php");
+1;2802;0cinclude_once("modele/function.php");
 
 $URL = 'http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&look_maxdist=150000&look_x=6112550&look_y=49610700&stationProxy=yes';
 $ch = curl_init();
