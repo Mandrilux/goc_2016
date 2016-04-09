32a900832a715b7fd39faaff439e5627f0670fb0
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 13:00:26 +0200

    fix

--- bus.php
+++ bus.php
@@ -16,7 +16,7 @@ foreach ($elements as $data)
   if ($l)
   {
     echo "sub";
-    $data = substr($data, 1,strlen($data) - 1);
+    $data = substr($data, 10,strlen($data) - 10);
     }
   echo '['.$data.']';
 //  $URL = 'http://travelplanner.mobiliteit.lu/restproxy/departureBoard?accessId=cdt&format=json&'.$data;
