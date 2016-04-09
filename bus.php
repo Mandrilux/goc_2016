3096ccef324c2f5d3ddbda8edd98270be78bbf3b
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 08:55:24 +0200

    decode

--- bus.php
+++ bus.php
@@ -3,15 +3,16 @@
 
 
 
-$wikipediaURL = 'http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes&look_maxdist=150&look_x=6112550&look_y=49610700';
+$URL = 'http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes&look_maxdist=150&look_x=6112550&look_y=49610700';
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $wikipediaURL);
+curl_setopt($ch, CURLOPT_URL, $URL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, '');
 $resultat = curl_exec ($ch);
 curl_close($ch);
 $resultat = json_decode($resultat);
 var_dump($resultat);
+
 //$dataJ = my_get('http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes&look_maxdist=150&look_x=6112550&look_y=49610700');
 //$dataJ = json_decode($dataJ);
 ?>
