1d8a35a04490518e133542f8ab2bc9a89baeee5d
lucas duval <lucas.duval@epitech.eu>
2016-04-09 04:20:48 +0200

    mvc

---
+++ controleur/index/index.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once('modele/function.php');
+
+include_once('vue/index/index.php');
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
-include("modele/connexion_sql.php");
-?>
-<p>coucou toi2 lol</p>
+include_once("modele/connexion_sql.php");
+if(!isset($_GET['section']))
+  {
+    include_once("controleur/index/index.php");
+  }
