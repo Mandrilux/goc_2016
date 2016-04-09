9117686510489d8d2cea3f639e7f18cd24805bb9
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 10:10:31 +0200

    for

--- bus.php
+++ bus.php
@@ -2,15 +2,14 @@
 include_once("modele/function.php");
 
 $URL = 'http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&look_maxdist=150000&look_x=6112550&look_y=49610700&stationProxy=yes';
-//$URL = 'http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes&look_maxdist=150&look_x=6112550&look_y=49610700';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $URL);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, '');
 $resultat = curl_exec ($ch);
 curl_close($ch);
-//$resultat = json_decode($resultat);
-//var_dump($resultat);
 $elements = explode(";", $resultat);
-var_dump($elements);
+foreach ($elements as $data)
+  echo $data;
+//var_dump($elements);
 ?>
