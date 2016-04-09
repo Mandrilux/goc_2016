7f00c7eddba666bb9c61a9d2bfee74c9cd6fae26
lucas duval <lucas.duval@epitech.eu>
2016-04-09 09:53:21 +0200

    func

--- modele/function.php
+++ modele/function.php
@@ -85,7 +85,7 @@ function get_closest_station($longitude, $latitude)
 	    }
 	}
       $info = get_info_station($id_min_dist);
-      if ($info['nb_dispo'] == 0)
+      /* if ($info['nb_dispo'] == 0)
 	{
 	  $i = 0;
 	  $stop = 1;
@@ -95,7 +95,7 @@ function get_closest_station($longitude, $latitude)
 		unset($tab_data[$i]);
 	      $i++;
 	    }
-	}
+	    }*/
     }
   array_push($info, $min_dist);
   //var_dump($info);
