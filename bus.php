326de917425f03193905207883d62793fb447f64
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 08:58:38 +0200

    emacs_bug

--- bus.php
+++ bus.php
@@ -8,7 +8,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_USERAGENT, '');
 $resultat = curl_exec ($ch);
 curl_close($ch);
-$resultat = json_decode($resultat);
+//$resultat = json_decode($resultat);
 var_dump($resultat);
 
 //$dataJ = my_get('http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes&look_maxdist=150&look_x=6112550&look_y=49610700');
