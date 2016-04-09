3fe82b8773d53d42b40b8d46894f2b7be1b0e0c1
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 08:19:22 +0200

    bus

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once("modele/connexion_sql.php");
 if(!isset($_GET['section']))
-  {
-    include_once("controleur/index/index.php");
-  }
+{
+  include_once("controleur/index/index.php");
+}
