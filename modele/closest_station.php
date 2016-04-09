893d0bb6f856a86e27a1adf94e252250df82dc3e
picot_a <vincent1.picot@epitech.eu>
2016-04-09 05:35:27 +0200

    maj

--- modele/closest_station.php
+++ modele/closest_station.php
@@ -18,11 +18,8 @@ function get_closest_station($longitude, $latitude)
     $stop = 0;
     foreach($tab_data as $data2)
     {
-      //echo "lol";
       $dist = my_get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='.$latitude.','.$longitude.'&destinations='.$data2["latitude"].','.$data2["longitude"].'&key=AIzaSyCnd5XCv5ks4QDZUhkVbthRlTdrTeij5-Y');
       $dist = json_decode($dist);
-      //echo "<br><br>";
-      //var_dump($dist);
       $cur_dist = $dist->{'rows'}[0]->{'elements'}[0]->{'distance'}->{'value'};
       if (isset($min_dist) == false)
       {
@@ -35,14 +32,11 @@ function get_closest_station($longitude, $latitude)
       $min_dist = $cur_dist;
       }
     }
-    //echo $min_dist;
-    //echo $id_min_dist;
     $info = get_info_station($id_min_dist);
     if ($info['nb_dispo'] == 0)
     {
       $i = 0;
       $stop = 1;
-      echo "OMGOMGOMGOMGOMG";
       while($tab_data[$i])
       {
 	if ($tab_data[$i]['id'] == $id_min_dist)
@@ -51,10 +45,9 @@ function get_closest_station($longitude, $latitude)
       }
     }
   }
+  array_push($info, $min_dist);
   var_dump($info);
   return ($info);
 }
-
-get_closest_station(6, 49);
 
 ?>
