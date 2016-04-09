66e6c927909f4c3f7fd3506a35dca6c3c686b2e5
lucas duval <lucas.duval@epitech.eu>
2016-04-09 09:10:47 +0200

    map

--- vue/index/index.php
+++ vue/index/index.php
@@ -198,8 +198,8 @@ var station = new google.maps.LatLng(<?php echo $data['latitude'].', '.$data['lo
 
   function Route() {
 
-    var start = new google.maps.LatLng(49.607232, 6.121065);
-    var end = new google.maps.LatLng(49.607248, 6.321076);
+    var start = new google.maps.LatLng(<?php echo $_POST['latitude'].', '.$_POST['longitude']; ?>);
+    var end = new google.maps.LatLng(<?php echo $data['latitude'].', '.$data['longitude']; ?>);
     var request = {
       origin: start,
       destination: end,
