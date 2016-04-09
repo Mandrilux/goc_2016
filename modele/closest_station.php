d3c8663db11c6a58c02fe6ba5fb368c1db5cc14b
picot_a <vincent1.picot@epitech.eu>
2016-04-09 04:52:51 +0200

    maj

--- modele/closest_station.php
+++ modele/closest_station.php
@@ -8,14 +8,13 @@ function get_closest_station($longitude, $latitude)
   global $bdd;
   $data = $bdd->prepare('SELECT id, latitude, longitude FROM `stations_velo`');
   $data->execute();
-/*
-  $data2 = $data->fetch();
-  $tab_data = array($data2);
-  print_r($tab_data);
-  
-    $tab_data = array_push($tab_data, $data2);*/
-  //foreach($tab_data as $data2)
+
+//  $data2 = $data->fetch();
+  $tab_data = array();
+//  print_r($tab_data);
   while ($data2 = $data->fetch())
+    $tab_data = array_push($tab_data, $data2);
+  foreach($tab_data as $data2)
   {
     echo "lol";
     $dist = my_get('https://maps.googleapis.com/maps/api/distancematrix/json?origins='.$latitude.','.$longitude.'&destinations='.$data2["latitude"].','.$data2["longitude"].'&key=AIzaSyC5WbmOFch6mj7T1L6CXjRMJ0sjdJuFlpc');
