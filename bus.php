c2efbac74e19bd4b271c94c100c954c01bc9252c
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 08:49:13 +0200

    decode

--- bus.php
+++ bus.php
@@ -11,6 +11,7 @@ curl_setopt($ch, CURLOPT_USERAGENT, '');
 $resultat = curl_exec ($ch);
 curl_close($ch);
 var_dump($resultat);
+$resultat = json_decode($resultat);
 //$dataJ = my_get('http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes&look_maxdist=150&look_x=6112550&look_y=49610700');
 //$dataJ = json_decode($dataJ);
 ?>
