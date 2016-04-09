b69c6df2b421a99242c1c73772bf95961deee613
Corentin Grandmaire <corentin.grndmaire@epitech.eu>
2016-04-09 08:30:06 +0200

    finsite

--- vue/index/index.php
+++ vue/index/index.php
@@ -213,6 +213,7 @@ var station = new google.maps.LatLng(<?php echo $data[0]['latitude'].', '.$data[
       }
     });
   }
+google.maps.event.addDomListener(window, 'load', initialize);
     </script>
         <div id="gmap" style="width:100%;height:450px;"></div>
             </div>
