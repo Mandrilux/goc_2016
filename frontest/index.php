70df09ee90027b28cb986c332c5488b59f1444d7
Corentin Grandmaire <corentin.grndmaire@epitech.eu>
2016-04-09 03:17:54 +0200

    addcountup

--- frontest/index.php
+++ frontest/index.php
@@ -91,7 +91,7 @@ include_once('../modele/connexion_sql.php'); ?>
         var easingFn = function (t, b, c, d) {
           var ts = (t /= d) * t;
           var tc = ts * t;
-          return b + c * (tc * ts + -5 * ts * ts + 10 * tc + -10 * ts + 5 * t) / 1;
+          return b + c * (tc * ts + -5 * ts * ts + 10 * tc + -10 * ts + 5 * t) / 2;
         }
         var options = {
           useEasing : true,
