a32ef96a95bc11158c3eb35fb1f1de0ad97aaced
lucas duval <lucas.duval@epitech.eu>
2016-04-09 10:12:37 +0200

    test

---
+++ text.php
@@ -0,0 +1,11 @@
+<script>
+if (navigator.geolocation)
+  {
+    navigator.geolocation.getCurrentPosition(function(position)
+					     {
+					       alert("Latitude : " + position.coords.latitude + ", longitude : " + position.coords.longitude);
+					     });
+  }
+else
+  alert("Votre navigateur ne prend pas en compte la géolocalisation HTML5");
+</script>
