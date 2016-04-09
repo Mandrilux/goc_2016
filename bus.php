cdb8233498350a17592a215a6f2ef89fe03c7c94
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 10:39:29 +0200

    result

--- bus.php
+++ bus.php
@@ -18,7 +18,7 @@ foreach ($elements as $data)
   curl_setopt($ch2, CURLOPT_USERAGENT, '');
   $resultat = curl_exec ($ch2);
   curl_close($ch2);
-  echo $URL;
+  echo $resultat;
   echo "<br />";
   //  var_dump($resultat);
 }
