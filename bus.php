97edf3674faaf05122adb630db22115852cc8065
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 08:41:11 +0200

    decode

--- bus.php
+++ bus.php
@@ -1,5 +1,6 @@
 <?php
 include_once("modele/function.php");
-$dataJ = my_get('http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes &look_maxdist=150&look_x=6112550&look_y=49610700');
+$dataJ = my_get('http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes&look_maxdist=150&look_x=6112550&look_y=49610700');
+$dataJ = json_decode($dataJ);
 var_dump($dataJ);
 ?>
