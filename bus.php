ab8f03bc980e52b5eec627848ba150d94f553e36
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 12:51:25 +0200

    fix

--- bus.php
+++ bus.php
@@ -16,12 +16,12 @@ foreach ($elements as $data)
   //if ($l)
     //$data = substr($data,1);
   echo '['.$data.']';
-  $URL = 'http://travelplanner.mobiliteit.lu/restproxy/departureBoard?accessId=cdt&format=json&'.$data;
-  curl_setopt($ch2, CURLOPT_URL, $URL);
-  curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch2, CURLOPT_USERAGENT, '');
-  $resultat = curl_exec ($ch2);
-  curl_close($ch2);
+//  $URL = 'http://travelplanner.mobiliteit.lu/restproxy/departureBoard?accessId=cdt&format=json&'.$data;
+//  curl_setopt($ch2, CURLOPT_URL, $URL);
+//  curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
+//  curl_setopt($ch2, CURLOPT_USERAGENT, '');
+//  $resultat = curl_exec ($ch2);
+//  curl_close($ch2);
   //echo $URL;
   echo "<br />";
   //$l = 1;
