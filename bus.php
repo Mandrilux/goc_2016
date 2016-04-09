99b50b6325a73bd5d04ef49c1db1889788ea6395
epitech2020 <baptiste.heraud@epitech.eu>
2016-04-09 08:34:48 +0200

    bus_fix

--- bus.php
+++ bus.php
@@ -1,4 +1,4 @@
 <?php
-$dataJ = my_get('http://opendata.vdl.lu/odaweb/index.jsp?describe=1');
+$dataJ = my_get('http://travelplanner.mobiliteit.lu/hafas/query.exe/dot?performLocating=2&tpl=stop2csv&stationProxy=yes &look_maxdist=150&look_x=6112550&look_y=49610700');
 var_dump($dataJ);
 ?>
